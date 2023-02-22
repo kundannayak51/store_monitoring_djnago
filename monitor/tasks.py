13c8482ac5372ba0e957df77c538ce95f5b3eefb
Kundan <kundan.nayak@apna.co>
2023-02-22 10:50:26 +0530

    WIP

--- monitor/tasks.py
+++ monitor/tasks.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+from random import random
 
 import pytz
 from celery import shared_task
 from django.db.models import Q
 
-from monitor.constants import STATUS_COMPLETE, STATUS_NONE
+from monitor.constants import STATUS_COMPLETE, STATUS_NONE, STATUS_ACTIVE, STATUS_INACTIVE
 from monitor.models import ReportStatus, StoreBusinessHours, StoreStatus
-from monitor.utils import CURRENT_TIME, get_time_of_x_days_before, LOCAL_TIME_START, LOCAL_TIME_END
+from monitor.utils import CURRENT_TIME, get_time_of_x_days_before, LOCAL_TIME_START, LOCAL_TIME_END, \
+    convert_utc_to_local, get_day_mapping, check_time_lies_between_two_time, get_chunk_number_from_end
 
 
 @shared_task
@@ -45,6 +47,64 @@ def calculate_weekly_observation_and_generate_report(report_id, store_id, store_
             hour_status[i] = STATUS_NONE
         status_map[day_of_week] = hour_status
 
+    for store_status in store_statuses:
+        timestamp_utc = store_status['timestamp_utc']
+        status = store_status['status']
+
+        local_time, day_str = convert_utc_to_local(timestamp_utc, timezone)
+
+        day = get_day_mapping(day_str)
+
+        lies_between, ex = check_time_lies_between_two_time(day_time_mapping[day][0], day_time_mapping[day][1], local_time)
+
+        if ex is not None:
+            #log error and return
+
+        if not lies_between:
+            continue
+
+        chunk_number = get_chunk_number_from_end(day_time_mapping[day][1], local_time)
+        if chunk_number is -1:
+            #log error and return
+        status_map[day][chunk_number] = status
+
+    status_map = enrich_status_map_with_nearest_status(status_map)
+
+    #get today's day
+    current_time_str = CURRENT_TIME
+    current_time_local, day_str = convert_utc_to_local(current_time_str, timezone)
+    day = get_day_mapping(day_str)
+
+
+
+
+def create_weekly_observation(store_id, status_map, current_day):
+    weekly_observation = []
+    is_last_hour_active = False
+
+    for key, val in status_map.items():
+        total_chunks = len(val)
+        active_status = 0
+
+        for chunk, status in val.items():
+            if status == STATUS_ACTIVE:
+                active_status += 1
+                if key == current_day and chunk == 0:
+                    is_last_hour_active = True
+
+        day_observation = {
+            'day': key,
+            'total_chunks': total_chunks,
+            'active_chunks': active_status
+        }
+        weekly_observation.append(day_observation)
+
+    return {
+        'store_id': store_id,
+        'week_report': weekly_observation,
+        'is_last_hour_active': is_last_hour_active
+    }
+
 
 def enrich_business_hour_and_return_day_time_mapping(store_id, business_hours):
     day_time_mapping = {}
@@ -75,6 +135,61 @@ def get_store_business_hours(store_id):
     business_hours = StoreBusinessHours.objects.filter(store_id=store_id)
 
     return list(business_hours.values('store_id', 'day_of_week', 'start_time_local', 'end_time_local'))
+
+
+def enrich_status_map_with_nearest_status(status_map):
+    result_map = {}
+    for key, val in status_map.items():
+        last_status = STATUS_INACTIVE
+        rand_val = random.random()
+        if rand_val >= 0.5:
+            last_status = STATUS_ACTIVE
+        last_status_index = 0
+        count_active = 0
+        count_inactive = 0
+        temp_map = {}
+        for i in range(len(val)):
+            if val.get(i) == STATUS_INACTIVE:
+                count_inactive += 1
+            elif val.get(i) == STATUS_ACTIVE:
+                count_active += 1
+
+        if count_active == 0 and count_inactive == 0:
+            for i in range(len(val)):
+                rand_val = random.random()
+                if rand_val >= 0.5:
+                    temp_map[i] = STATUS_ACTIVE
+                else:
+                    temp_map[i] = STATUS_INACTIVE
+
+        for i in range(len(val)):
+            if val.get(i) != STATUS_NONE:
+                last_status = val.get(i)
+                last_status_index = i
+                temp_map[i] = val.get(i)
+            else:
+                for j in range(i + 1, len(val)):
+                    if j - i > i - last_status_index:
+                        temp_map[i] = last_status
+                        break
+                    else:
+                        if val.get(j) != STATUS_NONE:
+                            if j - i == i - last_status_index:
+                                if count_inactive > count_active:
+                                    temp_map[i] = STATUS_INACTIVE
+                                else:
+                                    temp_map[i] = STATUS_ACTIVE
+                                break
+                            temp_map[i] = val.get(j)
+                            break
+                else:
+                    if count_inactive > count_active:
+                        temp_map[i] = STATUS_INACTIVE
+                    else:
+                        temp_map[i] = STATUS_ACTIVE
+        result_map[key] = temp_map
+    return result_map
+
 
 def calculate_total_chunks(start_time_str, end_time_str):
     start_time = datetime.strptime(start_time_str, '%H:%M:%S')
--- monitor/utils.py
+++ monitor/utils.py
@@ -1,6 +1,6 @@
 import base64
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 import pytz
 
@@ -37,3 +37,56 @@ def convert_utc_to_local(utc_time, timezone):
     day = local_time.strftime("%A")
 
     return local_time.strftime("%H:%M:%S"), day
+
+def get_day_mapping(day):
+    if day == "Monday":
+        return 0
+    elif day == "Tuesday":
+        return 1
+    elif day == "Wednesday":
+        return 2
+    elif day == "Thursday":
+        return 3
+    elif day == "Friday":
+        return 4
+    elif day == "Saturday":
+        return 5
+    elif day == "Sunday":
+        return 6
+    else:
+        return -1
+
+def check_time_lies_between_two_time(start_time_str, end_time_str, local_time_str, timezone):
+    try:
+        # Load the timezone
+        loc = time.timezone(timezone)
+    except Exception as e:
+        # Log error
+        return False, e
+
+    # Parse the start, end, and local time
+    start_time = time.strptime(start_time_str, "%H:%M:%S")
+    end_time = time.strptime(end_time_str, "%H:%M:%S")
+    local_time = time.strptime(local_time_str, "%H:%M:%S")
+
+    # Extract the hour, minute, and second from the times
+    local_hour, local_minute, local_second = local_time.tm_hour, local_time.tm_min, local_time.tm_sec
+    start_hour, start_minute, start_second = start_time.tm_hour, start_time.tm_min, start_time.tm_sec
+    end_hour, end_minute, end_second = end_time.tm_hour, end_time.tm_min, end_time.tm_sec
+
+    if local_hour >= start_hour and local_hour <= end_hour and \
+            ((local_hour > start_hour or (local_hour == start_hour and local_minute >= start_minute and local_second >= start_second)) and
+             (local_hour < end_hour or (local_hour == end_hour and local_minute <= end_minute and local_second <= end_second))):
+        return True, None
+
+    return False, None
+
+def get_chunk_number_from_end(end_time_str, input_time_str):
+    try:
+        input_time = datetime.datetime.strptime(input_time_str, '%H:%M:%S')
+        end_time = datetime.datetime.strptime(end_time_str, '%H:%M:%S')
+        minutes = int((end_time - input_time).total_seconds() / 60)
+        chunk = minutes // 60
+        return chunk
+    except ValueError:
+        return -1
